79e54a26ed17ce677474caf15682b3c8f850236e
Vale <Vale@DESKTOP-NFAMR29>
2024-04-26 01:21:45 +0200

    Creacion de paquetes y clases: Main y Producto para ejercicio1

---
+++ tp2VilteTomas/src/ar/edu/unju/fi/ejercicio1/main/Main.java
@@ -0,0 +1,8 @@
+package ar.edu.unju.fi.ejercicio1.main;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ tp2VilteTomas/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio1.model;
+
+public class Producto {
+	private int codigo;
+	private String descripcion;
+	private double precioUnitario;
+	private String origenFabricacion;
+	private String categoria;
+	
+	
+}
