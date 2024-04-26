5488d558dfd879dfff8fd0597f2b011361adf5ca
Vale <Vale@DESKTOP-NFAMR29>
2024-04-26 23:53:44 +0200

    Creacion de paquetes y clases: Main, Mes y Efemeride

---
+++ tp2VilteTomas/src/ar/edu/unju/fi/ejercicio2/main/Main.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.fi.ejercicio2.main;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ tp2VilteTomas/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio2.model;
+
+import ar.edu.unju.fi.ejercicio2.constantes.Mes;
+
+public class Efemeride {
+	private int codigo;
+	private Mes meses;
+	private int dia;
+	private String detalle;
+}
