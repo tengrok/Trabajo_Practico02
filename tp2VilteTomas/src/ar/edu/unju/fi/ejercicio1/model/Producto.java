5b9ca4dcb96bd22c4b344c553068bce1343712be
Vale <Vale@DESKTOP-NFAMR29>
2024-04-26 02:02:12 +0200

    Se completa la clase Producto

--- tp2VilteTomas/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
+++ tp2VilteTomas/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
@@ -4,8 +4,78 @@ public class Producto {
 	private int codigo;
 	private String descripcion;
 	private double precioUnitario;
-	private String origenFabricacion;
-	private String categoria;
+	private categoriaProducto categoria;
+	private origenFabricacion origen;
+	
+	public enum categoriaProducto{
+		TELEFONO, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+	}
+	
+	public enum origenFabricacion {
+		ARGENTINA, CHILE, BRASIL, URUGUAY
+	}
+
+	public Producto(int codigo, String descripcion, double precioUnitario, categoriaProducto categoria,
+			origenFabricacion origen) {
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioUnitario = precioUnitario;
+		this.categoria = categoria;
+		this.origen = origen;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public categoriaProducto getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(categoriaProducto categoria) {
+		this.categoria = categoria;
+	}
+
+	public origenFabricacion getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(origenFabricacion origen) {
+		this.origen = origen;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Codigo del Producto: ").append(getCodigo());
+		sb.append("Descripcion del Producto: ").append(getDescripcion());
+		sb.append("Precio Unitario del Producto: ").append(getPrecioUnitario());
+		sb.append("Categoria del Producto: ").append(getCategoria());
+		sb.append("Origen del Producto: ").append(getOrigen());
+		return sb.toString();
+	}
+	
+	
 	
 	
 }
