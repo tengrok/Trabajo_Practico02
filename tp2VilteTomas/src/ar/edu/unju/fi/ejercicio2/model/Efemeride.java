380b4e967e175e330dc057d353d2569ab5092bf8
Vale <Vale@DESKTOP-NFAMR29>
2024-04-27 00:02:29 +0200

    Se completa la clase Efemeride

--- tp2VilteTomas/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
+++ tp2VilteTomas/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
@@ -7,4 +7,57 @@ public class Efemeride {
 	private Mes meses;
 	private int dia;
 	private String detalle;
+	
+	public Efemeride(int codigo, Mes meses, int dia, String detalle) {
+		this.codigo = codigo;
+		this.meses = meses;
+		this.dia = dia;
+		this.detalle = detalle;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public Mes getMeses() {
+		return meses;
+	}
+
+	public void setMeses(Mes meses) {
+		this.meses = meses;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\nCodigo").append(getCodigo());
+		sb.append("\nMes").append(getMeses());
+		sb.append("\nDia").append(getDia());
+		sb.append("\nDetalle").append(getDetalle());
+		return sb.toString();
+	}
+	
+	
+	
+	
 }
